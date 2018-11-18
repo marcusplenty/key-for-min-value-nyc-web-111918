8830d059332342713e95255db0acb905012a72a6
capable-publisher-7225 <marcusplenty@gmail.com>
2018-11-18 17:31:09 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,9 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  if name_hash
+  if name_hash.empty? == true
+    return nil
+  end
   largest_value = 100000000
   largest_key = ""
   name_hash.each do |key, value|
