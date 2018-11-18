eac2bb9186d755e1154e52d0172667ad8cf89629
capable-publisher-7225 <marcusplenty@gmail.com>
2018-11-18 17:31:17 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,16 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  if name_hash.empty? == true
+    return nil
+  end
+  largest_value = 100000000
+  largest_key = ""
+  name_hash.each do |key, value|
+    if value < largest_value
+      largest_value = value 
+      largest_key = key
+    end
+  end
+  return largest_key
 end
